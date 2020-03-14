15be4747165dcdb6f77b779ee591e7ab23bf9dfc
RafaFigueiredo2203 <60237326+RafaelFigueiredo2203@users.noreply.github.com>
2020-03-13 22:02:45 -0300

    Update Cadastro-usuario.cpp

--- Cadastro-usuario C++/Cadastro-usuario.cpp
+++ Cadastro-usuario C++/Cadastro-usuario.cpp
@@ -1,39 +1,58 @@
 #include <stdio.h>
 #include <iostream>
+#include <string.h>
+
 int main( int argc, char** argv )
 {
-
+using namespace std;
 	setlocale(LC_ALL, "Portuguese");
 	int opcao;
 	char nome[50];
 	char tel[40];
+	
+	
 	printf ("\n Bem-Vindo \n\n");
 	printf ("1)Cadastro.");
 	printf ("\n2)Ajuda.");
 	printf ("\n3)Sair.");
 	
-	printf ("\n\nDigite a opção desejada :");
+	printf ("\n\nDigite a opÃ§Ã£o desejada :");
 	scanf ("%d",&opcao);
+
+
+
 	if(opcao){
 	
 	
 	 if(opcao == 2){
 		printf ("\nAjuda:");
-		printf ("\nDigite o o número referente a opção desejada , exemplo:");
-		printf ("\nNúmero 1:Realizar cadastro \nNúmero 2: Ajuda \nNúmero 3: Sair.");
+		printf ("\nDigite o o nÃºmero referente a opÃ§Ã£o desejada , exemplo:");
+		printf ("\nNÃºmero 1:Realizar cadastro \nNÃºmero 2: Ajuda \nNÃºmero 3: Sair.");
 	
-	printf ("\n\nDigite a opção desejada :");
+	printf ("\n\nDigite a opÃ§Ã£o desejada :");
 	scanf ("%d",&opcao);
 }
 	if(opcao == 1){
-	printf( "Digite seu nome:" );
-	scanf (" %[^\n]s",nome);
+		
+	REFAZ:fflush(stdin);printf( "\nDigite seu nome:" );
+	//scanf (" %[^\n]s",nome);
+	gets(nome);
+		if(strlen(nome)==0){
+		printf ("\n Erro campo nÃ£o preenchido.");
+		goto REFAZ;
+	}
+	REFAZ1:fflush(stdin);
 	printf ("\nDigite o telefone:");
-	scanf (" %[^\n]s",tel);
+	//scanf (" %[^\n]s",tel);
+	gets(nome);
+		if(strlen(nome)==0){
+		printf ("\n Erro campo nÃ£o preenchido.");
+		goto REFAZ1;
+	}
 }
 	else if (opcao == 3){
 	
-		printf ("\nEscolheu sair, até a próxima!");
+		printf ("\nEscolheu sair, atÃ© a prÃ³xima!");
 		
 	}
 }
